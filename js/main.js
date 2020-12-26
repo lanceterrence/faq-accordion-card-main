556edaa3fd056e2e1326fb112703820ddb12a84e
Lance Terrence <magsombol.lance@gmail.com>
2020-12-26 17:53:51 +0800

    Updated file

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// Questions
+var FAQ1 = document.getElementById("faq-one");
+var FAQ2 = document.getElementById("faq-two");
+var FAQ3 = document.getElementById("faq-three");
+var FAQ4 = document.getElementById("faq-four");
+var FAQ5 = document.getElementById("faq-five");
+
+// Items
+var ITEM1 = document.getElementsByClassName("one");
+var ITEM2 = document.getElementsByClassName("two");
+var ITEM3 = document.getElementsByClassName("three");
+var ITEM4 = document.getElementsByClassName("four");
+var ITEM5 = document.getElementsByClassName("five");
+
+
+
+function toggleList() {
+
+}
